7e73589372b74a7cfa3404904044adf0c0aa5a6d
Chris Gullickson <cgullickson5@gmail.com>
2018-01-04 00:28:57 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,90 @@
 require_relative "../config/environment.rb"
 
 class Student
+    attr_accessor :id, :name, :grade
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
+    def initialize (name, grade, id = nil)
+      @name = name
+      @grade = grade
+      @id = nil
+    end
 
+    def self.new_from_db(row)
+      new_student = Student.new
+      new_student.id = row[0]
+      new_student.name = row[1]
+      new_student.grade = row[2]
+      new_student
+      # create a new Student object given a row from the database
+    end
+
+    def self.create(name, grade)
+      new_student = Student.new(name,grade)
+      new_student.save
+      new_student
+    end
+
+    def self.all
+      sql = <<-SQL
+      SELECT *
+      FROM students
+      SQL
+      DB[:conn].execute(sql).map do |row|
+        self.new_from_db(row)
+      end
+
+      # retrieve all the rows from the "Students" database
+      # remember each row should be a new instance of the Student class
+    end
+
+    def self.find_by_name(name)
+      sql = <<-SQL
+        SELECT *
+        FROM students
+        WHERE name = ?
+        LIMIT 1
+      SQL
+
+      DB[:conn].execute(sql, name).map do |row|
+        self.new_from_db(row)
+      end.first
+      # find the student in the database given a name
+      # return a new instance of the Student class
+    end
+
+    def save
+      if self.id
+        self.update
+      else
+        sql = <<-SQL
+          INSERT INTO students (name, grade)
+          VALUES (?, ?)
+        SQL
+        DB[:conn].execute(sql, self.name, self.grade)
+        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+      end
+    end
+
+    def update
+      sql = "UPDATE students SET name = ?, grade = ? WHERE name = ?"
+      DB[:conn].execute(sql, self.name, self.grade, self.name)
+    end
+
+    def self.create_table
+      sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade TEXT
+      )
+      SQL
+
+      DB[:conn].execute(sql)
+    end
+
+    def self.drop_table
+      sql = "DROP TABLE IF EXISTS students"
+      DB[:conn].execute(sql)
+    end
 
 end
